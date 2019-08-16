97057474f2368ea4afb466a47971c0c5d19639e1
cx <xin.chen07@ucarinc.com>
2019-08-16 16:46:26 +0800

    hello world!

---
+++ src/main/java/com/ucar/training/controller/HomeController.java
@@ -0,0 +1,12 @@
+package com.ucar.training.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+    @RequestMapping("/")
+    public String getHome(){
+        return "home";
+    }
+}
