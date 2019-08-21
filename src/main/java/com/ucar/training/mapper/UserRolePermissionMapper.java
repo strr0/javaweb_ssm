02996aac9015a464ed582111498b915523052e70
cx <xin.chen07@ucarinc.com>
2019-08-21 17:35:25 +0800

    用户角色权限映射接口

--- src/main/java/com/ucar/training/mapper/UserRolePermissionMapper.java
+++ src/main/java/com/ucar/training/mapper/UserRolePermissionMapper.java
@@ -1,9 +1,17 @@
 package com.ucar.training.mapper;
 
+import com.ucar.training.entity.Permission;
+
 import java.util.List;
 
 public interface UserRolePermissionMapper {
-    List<String> getPermissions(String username);
+    List<Permission> getPermissions(String username);
     void insertUserRole(String username, String rolename);
     void updateUserRole(String rolename, String username);
+    void insertRolePermission(String name, String permission);
+    List<String> selectRolePermission(String name);
+
+    void deleteUser(String name);
+    void deleteRolePermission(String name);
+    void deleteRole(String name);
 }
