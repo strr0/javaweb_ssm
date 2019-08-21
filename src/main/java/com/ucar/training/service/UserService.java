4f4519541ee0eb4b1792b4696f69faab463a3de4
cx <xin.chen07@ucarinc.com>
2019-08-21 17:32:16 +0800

    用户服务接口

--- src/main/java/com/ucar/training/service/UserService.java
+++ src/main/java/com/ucar/training/service/UserService.java
@@ -1,4 +1,19 @@
 package com.ucar.training.service;
 
+import com.ucar.training.entity.Permission;
+import com.ucar.training.entity.User;
+import com.ucar.training.entity.UserForm;
+
+import java.util.List;
+
 public interface UserService {
+    boolean addUser(UserForm userForm);
+    boolean deleteUser(int id, String username);
+    boolean updateUser(UserForm userForm);
+    List<User> getUsers();
+    User getUserByName(String username);
+    User getUserById(int id);
+    User matchUser(String username, String password);
+    List<Permission> getPermissions(String username);
+    List<String> getRoles();
 }
