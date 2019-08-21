772261de1ab75be5f36c30b6d991d9212a477dd5
cx <xin.chen07@ucarinc.com>
2019-08-21 17:36:36 +0800

    角色控制器

---
+++ src/main/java/com/ucar/training/controller/RoleController.java
@@ -0,0 +1,50 @@
+package com.ucar.training.controller;
+
+import com.ucar.training.entity.Role;
+import com.ucar.training.service.impl.RoleServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import static org.springframework.web.bind.annotation.RequestMethod.*;
+
+@Controller
+public class RoleController {
+    @Autowired
+    private RoleServiceImpl service;
+
+    @RequestMapping("/role")
+    public String getRoles(Model model){
+        model.addAttribute("roles", service.getRoles());
+        return "role/role";
+    }
+
+    @RequestMapping(value = "/roleadd", method = GET)
+    public String getRoleForm(){
+        return "role/roleadd";
+    }
+
+    @RequestMapping(value = "/roleadd", method = POST)
+    public String postRoleForm(Role role){
+        service.addRole(role);
+        return "status/success";
+    }
+
+    @RequestMapping(value = "/rolechange", method = GET)
+    public String getChangeForm(String name, Model model){
+        model.addAttribute("roleKey", service.getRole(name));
+        return "role/rolechange";
+    }
+    @RequestMapping(value = "rolechange", method = POST)
+    public String postChangeForm(Role role){
+        service.updateRole(role);
+        return "status/success";
+    }
+
+    @RequestMapping(value = "/roledelete", method = POST)
+    public String deleteRole(String name, Model model){
+        service.deleteRole(name);
+        model.addAttribute("roles", service.getRoles());
+        return "role/role";
+    }
+}
