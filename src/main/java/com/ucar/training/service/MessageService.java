1207584a0899512a4b80135ae1582da3dd192a50
cx <xin.chen07@ucarinc.com>
2019-08-21 17:33:21 +0800

    留言服务接口

--- src/main/java/com/ucar/training/service/MessageService.java
+++ src/main/java/com/ucar/training/service/MessageService.java
@@ -1,4 +1,14 @@
 package com.ucar.training.service;
 
+import com.ucar.training.entity.Message;
+
+import java.util.List;
+
 public interface MessageService {
+    //添加留言
+    boolean addMessge(Message message);
+    //删除留言
+    boolean deleteMessage(int id);
+    //获取留言信息
+    List<Message> getMessages();
 }
