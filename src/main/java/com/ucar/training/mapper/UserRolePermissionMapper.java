8bcd86f939c7fe92c0e694751cd4c6496aa3e405
cx <xin.chen07@ucarinc.com>
2019-08-21 10:55:48 +0800

    UserRolePermission映射的接口

--- src/main/java/com/ucar/training/mapper/UserRolePermissionMapper.java
+++ src/main/java/com/ucar/training/mapper/UserRolePermissionMapper.java
@@ -4,4 +4,6 @@ import java.util.List;
 
 public interface UserRolePermissionMapper {
     List<String> getPermissions(String username);
+    void insertUserRole(String username, String rolename);
+    void updateUserRole(String rolename, String username);
 }
