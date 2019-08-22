44bffaa243d1613863aa97fbedf3a5b2b7ee3108
cx <xin.chen07@ucarinc.com>
2019-08-22 10:12:32 +0800

    权限映射接口

--- src/main/java/com/ucar/training/mapper/PermissionMapper.java
+++ src/main/java/com/ucar/training/mapper/PermissionMapper.java
@@ -5,5 +5,15 @@ import com.ucar.training.entity.Permission;
 import java.util.List;
 
 public interface PermissionMapper {
+    //插入权限
+    void insertPermission(Permission permission);
+    //删除权限
+    void deleteRolePermission(String name);
+    void deletePermission(String name);
+    //获取所有权限
     List<Permission> selectPermission();
+    //根据权限名获取权限信息
+    Permission getPermissionByName(String name);
+    //修改权限信息
+    void updatePermission(Permission permission);
 }
