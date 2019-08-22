9a749d7d7d3639a6207c71df1bb02b714f15d2ab
cx <xin.chen07@ucarinc.com>
2019-08-22 10:12:16 +0800

    权限服务接口

--- src/main/java/com/ucar/training/service/PermissionService.java
+++ src/main/java/com/ucar/training/service/PermissionService.java
@@ -1,4 +1,18 @@
 package com.ucar.training.service;
 
+import com.ucar.training.entity.Permission;
+
+import java.util.List;
+
 public interface PermissionService {
+    //添加权限
+    void addPermission(Permission permission);
+    //删除权限
+    void deletePermission(String name);
+    //获取所有权限
+    List<Permission> getPermissions();
+    //根据权限名获取权限
+    Permission getPermission(String name);
+    //修改权限
+    void changePermission(Permission permission);
 }
