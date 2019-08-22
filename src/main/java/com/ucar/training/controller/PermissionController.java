2111a8ac074439a9084c25b9c7b59f0d491923d1
cx <xin.chen07@ucarinc.com>
2019-08-22 10:13:15 +0800

    权限控制器

--- src/main/java/com/ucar/training/controller/PermissionController.java
+++ src/main/java/com/ucar/training/controller/PermissionController.java
@@ -1,5 +1,6 @@
 package com.ucar.training.controller;
 
+import com.ucar.training.entity.Permission;
 import com.ucar.training.service.impl.PermissionServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -13,7 +14,35 @@ public class PermissionController {
     private PermissionServiceImpl service;
 
     @RequestMapping(value = "/permission", method = GET)
-    public String getPermissionForm(Model model){
+    public String getPermissions(Model model){
+        model.addAttribute("permissionKey", service.getPermissions());
+        return "permission/permission";
+    }
+
+    @RequestMapping(value = "/permissionadd", method = GET)
+    public String getPermissionForm(){
+        return "permission/permissionadd";
+    }
+    @RequestMapping(value = "/permissionadd", method = POST)
+    public String postPermissionForm(Permission permission){
+        service.addPermission(permission);
+        return "status/success";
+    }
+
+    @RequestMapping(value = "/permissionchange", method = GET)
+    public String getChangeForm(String name, Model model){
+        model.addAttribute("permissionKey", service.getPermission(name));
+        return "permission/permissionchange";
+    }
+    @RequestMapping(value = "/permissionchange", method = POST)
+    public String postChangeForm(Permission permission){
+        service.changePermission(permission);
+        return "status/success";
+    }
+
+    @RequestMapping(value = "/permissiondelete", method = POST)
+    public String deletePermission(String name, Model model){
+        service.deletePermission(name);
         model.addAttribute("permissionKey", service.getPermissions());
         return "permission/permission";
     }
